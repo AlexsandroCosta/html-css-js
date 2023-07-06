359e55553f5f8cf487cc02c22aa00dd92d95ab43
AlexsandroCosta <alexsandro.costasv@gmail.com>
2023-07-06 16:11:34 -0300

    Diz vencedor

--- pedra_papel_tesoura/script.js
+++ pedra_papel_tesoura/script.js
@@ -4,6 +4,7 @@ document.getElementById("jogar").addEventListener('click', function(){
     let opc = document.getElementById("jogada").value
     let img1 = document.getElementById("img1")
     let img2 = document.getElementById("img2")
+    let res = document.getElementById("res")
 
     if(opc != ""){
         let x = Math.floor(Math.random() * 3)
@@ -19,29 +20,29 @@ document.getElementById("jogar").addEventListener('click', function(){
         if(opc == "pedra"){
             img1.src = "imagens/pedra.png"
             if(x == 0){
-                alert("Empate!")
+                res.innerHTML = "Empate!"
             }else if(x == 1){
-                alert("O computador venceu!")
+                res.innerHTML = "O computador venceu!"
             }else{
-                alert("O jogador venceu!")
+                res.innerHTML = "O jogador venceu!"
             }
         }else if(opc == "papel"){
             img1.src = "imagens/papel.png"
             if(x == 0){
-                alert("O jogador venceu!")
+                res.innerHTML = "O jogador venceu!"
             }else if(x == 1){
-                alert("Empate!")
+                res.innerHTML = "Empate!"
             }else{
-                alert("O computador venceu!")
+                res.innerHTML = "O computador venceu!"
             }
         }else{
             img1.src = "imagens/tesoura.png"
             if(x == 0){
-                alert("O computador venceu!")
+                res.innerHTML = "O computador venceu!"
             }else if(x == 1){
-                alert("O jogador venceu!")
+                res.innerHTML = "O jogador venceu!"
             }else{
-                alert("Empate!")
+                res.innerHTML = "Empate!"
             }
         }
     }else{
