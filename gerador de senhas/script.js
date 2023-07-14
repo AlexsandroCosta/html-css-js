ab1fa1beb95889b6730857504657fb3d4ee203d4
AlexsandroCosta <alexsandro.costasv@gmail.com>
2023-07-14 16:35:56 -0300

    gerador de senhas

--- gerador de senhas/script.js
+++ gerador de senhas/script.js
@@ -1,1 +1,22 @@
- 
+
+
+document.getElementById("gerar").addEventListener('click', function(){
+    let senha = ""
+
+    for(let i=0; i<8; i++){
+        if(Math.floor(Math.random()*2+1)==1){
+            senha += Math.floor(Math.random()*10)
+        }else{
+            let x = String.fromCharCode(Math.floor(Math.random()*26+1)+64)
+
+            if(Math.floor(Math.random()*2+1)==1){
+                senha += x.toLowerCase()
+            }else{
+                senha += x
+            }
+        }
+    }
+    let res = document.getElementById("res")
+    res.style.padding = "10px"
+    res.innerHTML = senha
+})
